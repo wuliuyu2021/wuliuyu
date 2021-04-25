4b92e61956e1d3f29587dd11c0c43f17d5eb2499
wuliuyu <wuliuyu@haplox.com>
2021-04-25 15:08:11 +0800

    first

--- python/stats_json_info.py
+++ python/stats_json_info.py
@@ -31,7 +31,7 @@ def info_write(indir, outdir):
 		f=open(sj)
 		data=json.load(f)
 		#for sample in data["ConversionResults"]["DemuxResults"]["SampleName"]:
-		print(data["ConversionResults"]["DemuxResults"]["SampleName"])
+		print(data["ConversionResults"]["DemuxResults"]["SampleName"][0])
 
 def main():
 	args = getCommands()
