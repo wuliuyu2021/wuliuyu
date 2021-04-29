c6cf8c38b43ca9934edcdadbd7bea15c5991fe73
wuliuyu <wuliuyu@haplox.com>
2021-04-29 17:12:48 +0800

    first

--- shell/NGSCheckMate.sh
+++ shell/NGSCheckMate.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
-infile=$1
+echo;date
+vcfdir=$1
 outdir=$2
+rm -f $outdir/list
+if [ ! -d $outdir/ngscheckmate ];then
+mkdir -p $outdir/ngscheckmate
+fi
+for vcfd in `ls -d $vcfdir/*`
 
-rm -f $outdir/tmp.txt
-for info in `cat $infile`
 do
-
-#fq=$(echo $info |awk -F "," '{print $2}')
-#sample=$(echo $info |awk -F "/" '{print $NF}')
-ossdir=$(ossutil ls  $info |tail -n 4 |head -n 1 |awk -F "_R1_001.fastq.gz" '{print $1}' |awk -F "oss" '{print "oss"$2}' )
-sample=$(echo $ossdir |awk -F "/" '{print $NF}')
-echo "${sample},${ossdir}" >> $outdir/tmp.txt
+sample = $(echo $vcfd |awk -F"/" '{print $NF}')
+sample_vcf=${vcfd}/${vcfd}.vcf
+echo $sample_vcf >> $outdir/list
 done
-python /data/users/wuliuyu/wuliuyu/python/kefu_fastp_merge_csv_info_v2.py -i $outdir/tmp.txt -o $outdir
+/usr/bin/python /thinker/nfs5/public/laigr/bin/NGSCheckMate/ncm.py -V -f  -l $outdir/list -N NAG -bed /thinker/nfs5/public/laigr/bin/NGSCheckMate/SNP/SNP_GRCh37_hg19_woChr.bed -O $outdir/ngscheckmate
+echo;date
