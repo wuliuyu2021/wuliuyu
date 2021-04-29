176eaa5d42b0650c5e1b9b46dc4e83d48e75376c
wuliuyu <wuliuyu@haplox.com>
2021-04-29 17:17:25 +0800

    first

--- shell/NGSCheckMate.sh
+++ shell/NGSCheckMate.sh
@@ -10,7 +10,7 @@ for vcfd in `ls -d $vcfdir/*`
 
 do
 sample = $(echo $vcfd |awk -F"/" '{print $NF}')
-sample_vcf=${vcfd}/${vcfd}.vcf
+sample_vcf=${vcfd}/${sample}.vcf
 echo $sample_vcf >> $outdir/list
 done
 /usr/bin/python /thinker/nfs5/public/laigr/bin/NGSCheckMate/ncm.py -V -f  -l $outdir/list -N NAG -bed /thinker/nfs5/public/laigr/bin/NGSCheckMate/SNP/SNP_GRCh37_hg19_woChr.bed -O $outdir/ngscheckmate
