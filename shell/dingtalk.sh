66da40d54d41df0d6bacaf927fb812162ccd9690
wuliuyu <wuliuyu@haplox.com>
2021-05-03 08:09:15 +0800

    first

--- shell/dingtalk.sh
+++ shell/dingtalk.sh
@@ -3,7 +3,7 @@ BG=$(df -h /home |tail -n 1 |awk -F" " '{print $3}' |awk -F"G" '{print $1}')
 echo $BG
 while [ 0 ];
 do
-if [ BG -gt 100 ];then
+if [ $BG -gt 100 ];then
 
 echo "echo "gm55a-1 /home ram unusual!""
 python3 /thinker/nfs5/public/wuliuyu/wuliuyu/python/dingtalkChatbot.py gm55a-1\ home\ ram\ larger\ than\ 100G! wly
