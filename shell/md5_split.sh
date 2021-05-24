56b55f8dfa0275bd4a5a2d9e84f728aca2fb9290
wuliuyu <wuliuyu@haplox.com>
2021-05-24 17:39:02 +0800

    first

--- shell/md5_split.sh
+++ shell/md5_split.sh
@@ -11,18 +11,18 @@ fi
 #fq5=$(cat $md5old |awk -F "  " '{print $2}' |awk -F "_" '{print $5}')
 for fqcode in `cat $md5old`;
 do
-code=$(awk -F"  " '{print $1}' $fqcode)
-fqR1=$(awk -F"  " '{print $2}' $fqcode |grep "_R1")
-fqR2=$(awk -F"  " '{print $2}' $fqcode |grep "_R2")
-fqR3=$(awk -F"  " '{print $2}' $fqcode |grep "_R3")
+code=$(echo $fqcode |awk -F"  " '{print $1}')
+fqR1=$(echo $fqcode |awk -F"  " '{print $2}' |grep "_R1")
+fqR2=$(echo $fqcode |awk -F"  " '{print $2}' |grep "_R2")
+fqR3=$(echo $fqcode |awk -F"  " '{print $2}' |grep "_R3")
 if [[ -f $fqR1 ]] && [[ ! -f $fqR2 ]] && [[ ! -f $fqR3 ]] ;then
-fq=$(awk -F"  " '{print $2}' $fqcode |awk -F"_" '{print $('$st')"_R1_001.fastq.gz"}')
+fq=$(echo $fqcode |awk -F"  " '{print $2}' |awk -F"_" '{print $('$st')"_R1_001.fastq.gz"}')
 echo "$code  $fq" >> $out/md5_new
 elif [[ ! -f $fqR1 ]] && [[ -f $fqR2 ]] && [[ ! -f $fqR3 ]] ;then
-fq=$(awk -F"  " '{print $2}' $fqcode |awk -F"_" '{print $('$st')"_R2_001.fastq.gz"}')
+fq=$(echo $fqcode |awk -F"  " '{print $2}' |awk -F"_" '{print $('$st')"_R2_001.fastq.gz"}')
 echo "$code  $fq" >> $out/md5_new
 elif [[ ! -f $fqR1 ]] && [[ ! -f $fqR2 ]] && [[ -f $fqR3 ]] ;then
-fq=$(awk -F"  " '{print $2}' $fqcode |awk -F"_" '{print $('$st')"_R3_001.fastq.gz"}')
+fq=$(echo $fqcode |awk -F"  " '{print $2}' |awk -F"_" '{print $('$st')"_R3_001.fastq.gz"}')
 echo "$code  $fq" >> $out/md5_new
 fi
 
