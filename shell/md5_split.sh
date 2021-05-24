ac9db2df624a362cee191b76fe071965b73e6a06
wuliuyu <wuliuyu@haplox.com>
2021-05-24 19:00:54 +0800

    first

--- shell/md5_split.sh
+++ shell/md5_split.sh
@@ -9,26 +9,26 @@ rm -f $out/${md5name}_new
 if [ ! -d $out ];then
 mkdir -p $out
 fi
-
-#fq5=$(cat $md5old |awk -F "  " '{print $2}' |awk -F "_" '{print $5}')
-for fqcode in `cat $md5old`;
+tmp=$out/${md5name}_new
+sed 's/  /CD==FQ/g' $md5old > $tmp
+for fqcode in `cat $tmp`;
 do
-code=$(echo $fqcode |awk -F"  " '{print $1}')
-fqR1=$(echo $fqcode |awk -F"  " '{print $2}' |grep "_R1")
-fqR2=$(echo $fqcode |awk -F"  " '{print $2}' |grep "_R2")
-fqR3=$(echo $fqcode |awk -F"  " '{print $2}' |grep "_R3")
-if [[ -n "$fqR1" ]];then
-fq=$(echo $fqcode |awk -F"  " '{print $2}' |awk -F"_" '{print $('$st')"_R1_001.fastq.gz"}')
+code=$(echo $fqcode |awk -F"CD==FQ" '{print $1}')
+fqR1=$(echo $fqcode |awk -F"CD==FQ" '{print $2}' |grep "_R1")
+fqR2=$(echo $fqcode |awk -F"CD==FQ" '{print $2}' |grep "_R2")
+fqR3=$(echo $fqcode |awk -F"CD==FQ" '{print $2}' |grep "_R3")
+if [[ -n "$fqR1" ]] && [[ -z "$fqR2" ]] && [[ -z "$fqR3" ]];then
+fq=$(echo $fqcode |awk -F"CD==FQ" '{print $2}' |awk -F"_" '{print $('$st')"_R1_001.fastq.gz"}')
 echo "$code  $fq" >> $out/${md5name}_new
 echo "$code  $fq"
-elif [[ -n "$fqR2" ]];then
-fq=$(echo $fqcode |awk -F"  " '{print $2}' |awk -F"_" '{print $('$st')"_R2_001.fastq.gz"}')
+elif [[ -z "$fqR1" ]] && [[ -n "$fqR2" ]] && [[ -z "$fqR3" ]];then
+fq=$(echo $fqcode |awk -F"CD==FQ" '{print $2}' |awk -F"_" '{print $('$st')"_R2_001.fastq.gz"}')
 echo "$code  $fq" >> $out/${md5name}_new
 echo "$code  $fq"
-elif [[ -n "$fqR3" ]] ;then
-fq=$(echo $fqcode |awk -F"  " '{print $2}' |awk -F"_" '{print $('$st')"_R3_001.fastq.gz"}')
+elif [[ -z "$fqR1" ]] && [[ -z "$fqR2" ]] && [[ -n "$fqR3" ]] ;then
+fq=$(echo $fqcode |awk -F"CD==FQ" '{print $2}' |awk -F"_" '{print $('$st')"_R3_001.fastq.gz"}')
 echo "$code  $fq" >> $out/${md5name}_new
 echo "$code  $fq"
 fi
-
+rm -f $tmp
 done
