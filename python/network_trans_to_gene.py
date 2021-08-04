c73fbfef23c79ccd52b965a4e2186c134fa1c574
wuliuyu <wuliuyu@haplox.com>
2021-08-04 14:41:52 +0800

    first

---
+++ python/network_trans_to_gene.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding = utf-8 -*-
+
+import os
+import sys
+import re
+#sys.argv[1]:All.KEGG_Enrich.xls
+#sys.argv[2]:pathwayname,hsa04010
+#sys.argv[3]:diff_file,log2FC1.0.Pvalue0.05.txt
+#sys.argv[4]:outdir
+lts=[]
+out=os.path.join("%s/%s.%s.diff.gene.xls" %(sys.argv[4], os.path.basename(sys.argv[1]), sys.argv[2]))#file name
+out_open=open(out,"w")
+for line in open(sys.argv[1], "r").readlines()[1:]:#path csv
+
+	lst=line.strip().split("\t")
+	if  len(sys.argv[2].split(",")) == 1 and lst[0] == sys.argv[2]:#gene ID
+		for gene in lst[7].split("/"):
+			lts.append(gene)
+	if len(sys.argv[2].split(",")) > 1 and lst[0] in sys.argv[2].split(","):
+		for path in sys.argv[2].split(","):
+			for gene in lst[7].split("/"):
+				lts.append(gene)
+print(lts)
+head_list=open(sys.argv[3], "r").readlines()[0]
+head="\t".join(head_list.split("\t"))
+out_open.write(head)
+for line in open(sys.argv[3], "r").readlines()[1:]:
+	lst=line.strip().split("\t")
+	if lst[0] in sorted(set(lts)):
+		out_open.write("%s\n" % "\t".join(lst))
+		
+out_open.close()
--- shell/nt_blast.sh
+++ shell/nt_blast.sh
@@ -2,7 +2,8 @@
 indir=$1
 outdir=$2
 num=$3
+blast_num=$4
 
 sh /thinker/nfs5/public/wuliuyu/wuliuyu/shell/2W_reads_split.sh $indir $outdir $num
-python /thinker/nfs5/public/wuliuyu/wuliuyu/python/blast_muti.py $outdir 4
+python /thinker/nfs5/public/wuliuyu/wuliuyu/python/blast_muti.py $outdir $blast_num
 sh /thinker/nfs5/public/wuliuyu/wuliuyu/shell/top3_blast_result.sh $outdir
