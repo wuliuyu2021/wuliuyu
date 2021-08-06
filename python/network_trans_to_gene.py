1cd02e1cf37ea9238476c83c8e29ba5b3ef77a7e
wuliuyu <wuliuyu@haplox.com>
2021-08-06 21:06:47 +0800

    first

--- python/network_trans_to_gene.py
+++ python/network_trans_to_gene.py
@@ -9,26 +9,41 @@ infile=sys.argv[1]
 indir=sys.argv[2]
 outdir=sys.argv[3]
 
-dt={}
-for line in open(infile, "r").readlines()[:]:#path csv
-	lst=line.strip().split("\t")
-	lst[0]=dt[lst[1]]
-print(dt)
-pattern = re.compile(r"(.+)(.network.txt)$")
-srs = sorted(filter(lambda x: re.match(pattern, x), os.listdir(indir)))
-for sr in srs:
-	file=os.path.join(outdir,"%s_result.txt" % os.path.basename(sr))
-	file_open=open(file, "w")
-	head=open(indir+"/"+sr, "r").readlines()[0]
-	file_open.write(head+"\n")
-	for i,element in enumerate(head):
-		x=""
-		if element == "to":
-			x=int(i)
-	for line in open(indir+"/"+sr, "r").readlines()[1:]:
+def dict_maker(infile):
+	dt={}
+	for line in open(infile, "r").readlines()[:]:#path csv
 		lst=line.strip().split("\t")
-		if lst[x] not in dt.keys():
-			file_open.write(line+"\n")
-		else:
-			file_open.write("\t".join(lst[0:x])+"\t"+"\t".join(lst[(x+1):]+"\n")
-	file_open.close()
+		lst[0]=dt[lst[1]]
+	return dt
+	print(dt)
+
+
+def info_write(indir,outdir):
+	dt=dict_maker(infile)
+	pattern = re.compile(r"(.+)(.network.txt)$")
+	srs = sorted(filter(lambda x: re.match(pattern, x), os.listdir(indir)))
+	for sr in srs:
+		file=os.path.join(outdir,"%s_result.txt" % os.path.basename(sr))
+		file_open=open(file, "w")
+		head=open(indir+"/"+sr, "r").readlines()[0]
+		file_open.write(head+"\n")
+		for i,element in enumerate(head):
+			x=""
+			if element == "to":
+				x=int(i)
+		for line in open(indir+"/"+sr, "r").readlines()[1:]:
+			lst=line.strip().split("\t")
+			if lst[x] not in dt.keys():
+				file_open.write(line+"\n")
+			else:
+				file_open.write("\t".join(lst[0:x])+"\t"+"\t".join(lst[(x+1):]+"\n"))
+		file_open.close()
+
+def main():
+	infile=sys.argv[1]
+	indir=sys.argv[2]
+	outdir=sys.argv[3]
+	info_write(indir,outdir)
+
+if __name__ == '__main__':
+	main()
