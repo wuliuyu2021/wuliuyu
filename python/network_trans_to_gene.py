68560a64daea79bde67fe64b9ec5b4f13cb18403
wuliuyu <wuliuyu@haplox.com>
2021-08-07 15:58:33 +0800

    first

--- python/network_trans_to_gene.py
+++ python/network_trans_to_gene.py
@@ -5,10 +5,6 @@ import os
 import sys
 import re
 
-infile=sys.argv[1]
-indir=sys.argv[2]
-outdir=sys.argv[3]
-
 def dict_maker(infile):
 	dt={}
 	for line in open(infile, "r").readlines()[:]:#path csv
@@ -17,11 +13,9 @@ def dict_maker(infile):
 	#print(dt)
 	return dt
 	
-
-
-def info_write(indir,outdir):
+def info_write(infile,indir,outdir):
 	dt=dict_maker(infile)
-	pattern = re.compile(r"(.+)(.network.txt)$")
+	pattern = re.compile(r"(.+)(network.txt)$")
 	srs = sorted(filter(lambda x: re.match(pattern, x), os.listdir(indir)))
 	for sr in srs:
 		dx={}
@@ -42,14 +36,14 @@ def info_write(indir,outdir):
 			if lst[y] not in dt.keys():
 				file_open.write(line)
 			else:
-				file_open.write("\t".join(lst[0:y])+"\t"+"\t".join(lst[(y+1):])+"\n")
+				file_open.write("\t".join(lst[0:y])+"\t"+lst[y]+"\t"+"\t".join(lst[(y+1):])+"\n")
 		file_open.close()
 
 def main():
 	infile=sys.argv[1]
 	indir=sys.argv[2]
 	outdir=sys.argv[3]
-	info_write(indir,outdir)
+	info_write(infile,indir,outdir)
 
 if __name__ == '__main__':
 	main()
