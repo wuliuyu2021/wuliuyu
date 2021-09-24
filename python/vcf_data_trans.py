ee90110ff4226702c40344269bded829abc46f50
wuliuyu <wuliuyu@haplox.com>
2021-09-24 20:59:12 +0800

    first

--- python/vcf_data_trans.py
+++ python/vcf_data_trans.py
@@ -11,7 +11,7 @@ outfile=os.path.join(out, "%s_tmp" % os.path.basename(csv))
 #	os.remove(outfile)
 outopen=open(outfile, "w")
 
-for line in open(csv, encoding='gbk').readlines()[0:]:
+for line in open(csv, "r").readlines()[0:]:
 	lst=line.strip().split(",")
 	if lst[27].find("vcf") != -1:
 		print("%s-%s-%s" % (lst[1],lst[16],lst[18]))
