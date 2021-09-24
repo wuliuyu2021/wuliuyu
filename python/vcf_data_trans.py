6afacc35eaefd5dcc8771b7fbcf4a0229d5371f9
wuliuyu <wuliuyu@haplox.com>
2021-09-24 21:30:34 +0800

    first

--- python/vcf_data_trans.py
+++ python/vcf_data_trans.py
@@ -7,6 +7,7 @@ csv=sys.argv[1]
 out=sys.argv[2]
 
 outfile=os.path.join(out, "%s_tmp" % os.path.basename(csv))
+#fcid=os.path.basename(csv).split(".csv")[0].split("sequence_")[-1]
 #if os.path.exists(outfile):
 #	os.remove(outfile)
 outopen=open(outfile, "w")
@@ -14,7 +15,7 @@ outopen=open(outfile, "w")
 for line in open(csv, "r", encoding='gbk').readlines()[0:]:
 	lst=line.strip().split(",")
 	if lst[27].find("vcf") != -1:
-		print("%s-%s-%s" % (lst[1],lst[16],lst[18]))
+		print("%s: %s-%s-%s" % (lst[0],lst[1],lst[16],lst[18]))
 		outopen.write("%s,%s-%s-%s,%s\n" % (lst[0],lst[1],lst[16],lst[18],",".join(lst[2:])))
 	else:
 		outopen.write(",".join(lst[0:])+"\n")
