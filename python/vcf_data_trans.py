04f9c7674f04a33ad7bcf2b52ea58641aeace55a
wuliuyu <wuliuyu@haplox.com>
2021-09-24 22:15:27 +0800

    first

--- python/vcf_data_trans.py
+++ python/vcf_data_trans.py
@@ -16,10 +16,18 @@ for line in open(csv, "r", encoding='gbk').readlines()[0:]:
 	lst=line.strip().split(",")
 	if lst[27].find("vcf") != -1:
 		print("There is vcf sample: %s" % lst[0])
-		if lst[2].find("%s-%s" % (str(lst[16]), str(lst[18]))) == 1:
-			outopen.write(",".join(lst[0:])+"\n")
-		else:	
-			outopen.write("%s,%s-%s-%s,%s\n" % (lst[0],lst[1],lst[16],lst[18],",".join(lst[2:])))
+		if lst[18] == "" and lst[16] != "":
+			if lst[2].find("%s" % str(lst[16])) == 1:
+				outopen.write(",".join(lst[0:])+"\n")
+			else:	
+				outopen.write("%s,%s-%s-%s,%s\n" % (lst[0],lst[1],lst[16],lst[18],",".join(lst[2:])))
+		if lst[18] != "" and lst[16] != "":
+			if lst[2].find("%s-%s" % (str(lst[16]), str(lst[18]))) == 1:
+				outopen.write(",".join(lst[0:])+"\n")
+			else:
+				outopen.write("%s,%s-%s-%s,%s\n" % (lst[0],lst[1],lst[16],lst[18],",".join(lst[2:])))
+		if lst[16] == "":
+			print("Warning, %s:sampletype is null" % lst[0])
 	else:
 		outopen.write(",".join(lst[0:])+"\n")
 outopen.close()
