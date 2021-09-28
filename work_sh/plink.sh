a782592094b2aede860c648315041a4572112da1
wuliuyu <wuliuyu@haplox.com>
2021-09-28 18:38:08 +0800

    first

--- work_sh/plink.sh
+++ work_sh/plink.sh
@@ -21,7 +21,7 @@ ls $sd/*_snp.vcf.gz | tr "\n" " " > $sr/info.csv
 #info_maf = $(echo $info)
 /thinker/nfs5/public/wuliuyu/Hapcolud_docker/bcftoolsVariant/bcftools-1.12/bcftools merge -o $sr/all.vcf.gz $(cat $sr/info.csv)
 
-less all.vcf.gz |sed 's/\s\.\/\.:\.:\./\t0\/0:\.:\./g' | awk '{if($1~/^#CHROM/){for(m=10;m<=NF;m++){if($m~/\//){match($m,/.*\/(.*.bam)/,a)}else{a[1]=$m}; b=b"\t"a[1]};print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9b}else{if($1!~/ID=KI|ID=GL|^KI|^GL/)print $0}}' > $sr/all.vcf
+less $sr/all.vcf.gz |sed 's/\s\.\/\.:\.:\./\t0\/0:\.:\./g' | awk '{if($1~/^#CHROM/){for(m=10;m<=NF;m++){if($m~/\//){match($m,/.*\/(.*.bam)/,a)}else{a[1]=$m}; b=b"\t"a[1]};print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9b}else{if($1!~/ID=KI|ID=GL|^KI|^GL/)print $0}}' > $sr/all.vcf
 
 cd $sr
 /thinker/nfs5/public/tools/plink_soft/plink --const-fid --vcf $sr/all.vcf --make-bed -out ./plink
