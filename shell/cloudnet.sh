f081bf14f515cab96f208f3f9bedebb9f3ab7632
wuliuyu <wuliuyu@haplox.com>
2021-10-21 10:22:58 +0800

    first

--- shell/cloudnet.sh
+++ shell/cloudnet.sh
@@ -5,6 +5,6 @@ indir=$1
 outseq=$(echo $indir | awk -F "/" '{print $(NF-1)}')
 piddir=$(echo $indir | awk -F "/" '{print $NF}')
 
-ossutil cp -ru ${indir}/ oss://sz-hapseq/rawfq/outseq-tech/${outseq}/${piddir}/
+ossutil cp -ru ${indir}/ oss://sz-hapseq/outseq-tech/${outseq}/${piddir}/
 
-echo "${indir} uploaded to oss://sz-hapseq/rawfq/outseq-tech/${outseq}/${piddir}/ done"
+echo "${indir} uploaded to oss://sz-hapseq/outseq-tech/${outseq}/${piddir}/ done"
