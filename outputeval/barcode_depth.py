b799808c2fc8c385d7a1247b25c0dea184be6dfc
wuliuyu <wuliuyu@haplox.com>
2021-10-28 13:40:04 +0800

    first

--- outputeval/barcode_depth.py
+++ outputeval/barcode_depth.py
@@ -33,7 +33,7 @@ def main():
             data_dir = info[3]  #bcl目录
             raw_dir = info[4] #拆分输出目录
             lane_num = info[5]
-            output_dir = raw_dir + batch + '_' + mode2 + '_barcode'
+            output_dir = raw_dir +"/" +batch + '_' + mode2 + '_barcode'
 #======================更改samplesheet===============            
             samplesheet_csv = []
             with open(sample_sheet_name,'r') as f:  #将samplesheet的reads从151改成1
