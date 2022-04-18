304c739604858c9caeb08cf10f15f7d931eea8fe
wuliuyu <wuliuyu@haplox.com>
2022-04-18 16:33:12 +0800

    first

--- shell/2W_reads_split.sh
+++ shell/2W_reads_split.sh
@@ -9,8 +9,11 @@ mkdir -p $outdir
 fi
 
 cd $indir
-for fq in `ls *_R1_001.fastq.gz` `ls *_R1.fastq.gz`;
+for fq in `ls *_R1_001.fastq.gz` `ls *_R1.fastq.gz` `ls *.filtered.1.fq.gz`;
 do
+flag1=$(echo $fq |awk -F "R1" '{print $NF}')
+flag2=$(echo $fq |awk -F ".filtered.1" '{print $NF}')
+if [ $flag1 == "_001.fastq.gz" ] || [ $flag1 == ".fastq.gz" ];then
 prefix1=$(echo $fq |awk -F "_R1" '{print $1}' |awk -F "_" '{print $1}')
 prefix2=$(echo $fq |awk -F "_R1" '{print $1}' |awk -F "_" '{print $2}')
 prefix3=$(echo $fq |awk -F "_R1" '{print $1}' |awk -F "_" '{print $3}')
@@ -26,5 +29,10 @@ less $indir/$fq |head -n 80000 > $outdir/${prefix3}.fastq
 fi
 if [ $num == "4" ];then
 less $indir/$fq |head -n 80000 > $outdir/${prefix4}.fastq
+fi
+fi
+if [ $flag2 == ".fq.gz" ];then
+prefix=$(echo $fq |awk -F "" '{print $1}' )
+less $indir/$fq |head -n 80000 > $outdir/${prefix}.fastq
 fi
 done
