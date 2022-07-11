0b1a174bf048a3b67b830ec7473d1d49fa9a24e7
wuliuyu <wuliuyu@haplox.com>
2022-07-11 11:47:13 +0800

    first

--- python/sample_same_cat.py
+++ python/sample_same_cat.py
@@ -32,6 +32,30 @@ def catdirs(rawdir1, rawdir2):
 				os.system('cat %s/%s >> %s/%s' % (rawdir1, sr1_2, rawdir2, sr2_2))
 				print('%s/%s merged with %s/%s' % (rawdir1, sr1_2, rawdir2, sr2_2))
 				i=i+1
+		ir1_1 = sr1_1.replace("_R1_001", "_I1_001")
+		ir1_2 = sr1_2.replace("_R2_001", "_I2_001")
+		if os.path.exists(ir1_1) and os.path.exists(ir1_2):
+			for sr2_1 in srs2:
+				sr2_2 = sr2_1.replace("_R1_001", "_R2_001")
+				ir2_1 = sr2_1.replace("_R1_001", "_I1_001")
+				ir2_2 = sr2_2.replace("_R2_001", "_I2_001")
+				if os.path.exists(ir2_1) and os.path.exists(ir2_2):
+					if ('%s' % ir1_1.split('_')[0]) == ('%s' % ir2_1.split('_')[0]):
+						os.system('cat %s/%s >> %s/%s' % (rawdir1, ir1_1, rawdir2, ir2_1))
+						print('%s/%s merged with %s/%s' % (rawdir1, ir1_1, rawdir2, ir2_1))
+						i=i+1
+						os.system('cat %s/%s >> %s/%s' % (rawdir1, ir1_2, rawdir2, ir2_2))
+						print('%s/%s merged with %s/%s' % (rawdir1, ir1_2, rawdir2, ir2_2))
+						i=i+1
+		if os.path.exists(ir1_1) and not os.path.exists(ir1_2):
+			for sr2_1 in srs2:
+				ir2_1 = sr2_1.replace("_R1_001", "_I1_001")
+				if os.path.exists(ir2_1):
+					if ('%s' % ir1_1.split('_')[0]) == ('%s' % ir2_1.split('_')[0]):
+						os.system('cat %s/%s >> %s/%s' % (rawdir1, ir1_1, rawdir2, ir2_1))
+						print('%s/%s merged with %s/%s' % (rawdir1, ir1_1, rawdir2, ir2_1))
+						i=i+1
+
 	print(i)
 
 
