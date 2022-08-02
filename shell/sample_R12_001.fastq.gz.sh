d25c284b7b45cef3eed4d05fa555f8d7a35923cf
wuliuyu <wuliuyu@haplox.com>
2022-08-02 10:23:53 +0800

    first

--- shell/sample_R12_001.fastq.gz.sh
+++ shell/sample_R12_001.fastq.gz.sh
@@ -5,23 +5,35 @@ st=$2
 
 cd $dir
 
-for fq in `ls *R1_001.fastq.gz` `ls *R1.fastq.gz` `ls *1.fq.gz`;
+for fq in `ls *R1_001.fastq.gz` `ls *R1.fastq.gz` `ls *R1.fq.gz`;
 do
 frn=$(ls $fq | awk -F "_" '{print $('$st')}')
 mv $fq ${frn}_R1_001.fastq.gz
 echo "$fq moves to ${frn}_R1_001.fastq.gz"
 done 
 
-for fq in `ls *R2_001.fastq.gz` `ls *R2.fastq.gz` `ls *2.fq.gz`;
+for fq in `ls *R2_001.fastq.gz` `ls *R2.fastq.gz` `ls *R2.fq.gz`;
 do
 frn=$(ls $fq | awk -F "_" '{print $('$st')}')
 mv $fq ${frn}_R2_001.fastq.gz
 echo "$fq moves to ${frn}_R2_001.fastq.gz"
 done
 
-for fq in `ls *R3_001.fastq.gz` `ls *R3.fastq.gz` `ls *3.fq.gz`;
+for fq in `ls *R3_001.fastq.gz` `ls *R3.fastq.gz` `ls *R3.fq.gz`;
 do
 frn=$(ls $fq | awk -F "_" '{print $('$st')}')
 mv $fq ${frn}_R3_001.fastq.gz
 echo "$fq moves to ${frn}_R3_001.fastq.gz"
+done
+for fq in `ls *I1_001.fastq.gz` `ls *I1.fastq.gz` `ls *I1.fq.gz`;
+do
+frn=$(ls $fq | awk -F "_" '{print $('$st')}')
+mv $fq ${frn}_I1_001.fastq.gz
+echo "$fq moves to ${frn}_I1_001.fastq.gz"
+done
+for fq in `ls *I2_001.fastq.gz` `ls *I2.fastq.gz` `ls *I2.fq.gz`;
+do
+frn=$(ls $fq | awk -F "_" '{print $('$st')}')
+mv $fq ${frn}_I2_001.fastq.gz
+echo "$fq moves to ${frn}_I2_001.fastq.gz"
 done
