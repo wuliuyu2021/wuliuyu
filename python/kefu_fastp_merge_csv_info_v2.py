6aee9cc9c8f6ac3372d5196f2260fe6af2a18766
wuliuyu <wuliuyu@haplox.com>
2022-12-08 11:49:16 +0800

    first

--- python/kefu_fastp_merge_csv_info_v2.py
+++ python/kefu_fastp_merge_csv_info_v2.py
@@ -78,7 +78,7 @@ def info_maker():
 					else:
 						f.write("%s_R1_001.fastq.gz,%s_R2_001.fastq.gz,%s\n" % (path, path, sample))
 			else:
-				print("\033[5;31;47mWrong, %s,%s can not find the values in dictionary, please check!\033[0m" % (sample_pre,path))
+				print("\033[1;31;44mWrong, %s,%s can not find the values in dictionary, please check!\033[0m" % (sample_pre,path))
 	f.close()
 
 def main():
